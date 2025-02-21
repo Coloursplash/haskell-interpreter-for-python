1a4b90c2415c02204c7f606829b55e063e9f08fe
Sean Robinson <seankrobinson123@gmail.com>
2025-02-21 19:37:50 +0000

    Wrote parseBlock and parseStmt (cannot test until parseExpr completed)

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -10,15 +10,45 @@ evaluate :: Through Block VarList
 evaluate = evalBlock []
 
 evalBlock :: VarList -> Through Block VarList
-evalBlock vars b = undefined
+evalBlock vars [] = Right vars
+evalBlock vars (s:b) = do 
+    vars' <- evalStmt vars s 
+    evalBlock vars' b
 
 evalStmt :: VarList -> Through Stmt VarList
-evalStmt vars stmt = undefined 
+evalStmt vars (Asgn str e) = do 
+    vars' <- evalExpr vars e 
+    Right $ update str vars' vars
+evalStmt vars stmt@(While e b) = do 
+    val <- evalExpr vars e
+    case val of 
+        FalseVal -> Right vars 
+        TrueVal ->  do 
+            vars' <- evalBlock vars b
+            evalStmt vars' stmt
+        x -> Left (EvaluationError $ TypeError ("Expected type Boolean but got " ++ showType x))
+evalStmt vars (Cond e b1 b2) = do 
+    val <- evalExpr vars e 
+    case val of 
+        TrueVal -> do
+            vars' <- evalBlock vars b1 
+            Right vars' 
+        FalseVal -> do 
+            vars' <- evalBlock vars b2 
+            Right vars' 
+evalStmt vars (ExprStmt e) = do 
+    val <- evalExpr vars e 
+    Right vars
 
-evalExpr :: VarList -> Through Expr VarList
+ 
+
+evalExpr :: VarList -> Through Expr Val
 evalExpr vars (Add e1 e2) = undefined 
 
-evalFunc :: VarList -> Through Block VarList
+-- technically should have type VarList -> Through Block [Val]
+-- however trying to get it working for simple programs first before dealing 
+-- with more complex things like that
+evalFunc :: VarList -> Through Block Val
 evalFunc vars b = undefined
 
 update :: String -> Val -> VarList -> VarList
